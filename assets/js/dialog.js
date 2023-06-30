93638e2efe82fab88a62c31a3a8b8e61aa44e8af
shiliyahamster <shiliyahamster@gmail.com>
2023-07-01 01:18:12 +0800

    Change: update code formate and add custom img/color

---
+++ assets/js/dialog.js
@@ -0,0 +1,22 @@
+import SampleDB from "./sampleDBscript.js";
+let btn = document.querySelector("#show");
+let infoModal = document.querySelector("#infoModal");
+let close = document.querySelector("#close");
+let colorPickers = document.querySelectorAll(".colorpicker");
+let profile = JSON.parse(localStorage.getItem("profile"));
+let colorArray = JSON.parse(localStorage.getItem("profile")).colorArray;
+const sampleDB = new SampleDB();
+colorPickers.forEach((colorPicker, index) => {
+  colorPicker.value = colorArray[index];
+  colorPicker.addEventListener("change", (e) => {
+    colorArray[index] = e.target.value;
+    profile.colorArray = colorArray;
+    sampleDB.updateJson(profile)
+  });
+});
+btn.addEventListener("click", function () {
+  infoModal.showModal();
+});
+close.addEventListener("click", function () {
+  infoModal.close();
+});
